433c307b87f28c9e42d0fa09e9231b788826a1a0
Ghedo <federicoghedini@gmail.com>
2024-07-08 12:12:05 +0200

    chore: Add footer component to app component template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,19 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/ui/header.component';
+import { FooterComponent } from './shared/ui/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  host: {
+    class: 'block flex flex-col justify-start h-full',
+  },
   template: `
     <app-header></app-header>
     <router-outlet></router-outlet>
+    <app-footer></app-footer>
     `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
