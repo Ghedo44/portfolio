224b54404c30c7592799d453a3f76d0894000fdf
Ghedo <federicoghedini@gmail.com>
2024-07-10 18:23:33 +0200

    refactor: Update app.config.ts to use ReCaptchaV3Provider for Firebase app check

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { afterNextRender, ApplicationConfig, inject, Injector, PLATFORM_ID } from '@angular/core';
+import { afterNextRender, ApplicationConfig, Inject, inject, Injectable, InjectionToken, Injector, Optional, PLATFORM_ID } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,8 +6,29 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { getApp, initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
-import { initializeAppCheck, provideAppCheck, ReCaptchaV3Provider } from '@angular/fire/app-check';
-import { isPlatformServer } from '@angular/common';
+import { CustomProvider, initializeAppCheck, provideAppCheck, ReCaptchaV3Provider } from '@angular/fire/app-check';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+
+// import type { app } from 'firebase-admin';
+// export const FIREBASE_ADMIN = new InjectionToken<app.App>('firebase-admin');
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecaptchaBrowser {
+
+  constructor(
+      @Inject(PLATFORM_ID) private platformId: string
+  ) { }
+
+  provider(siteKey: string) {
+      return isPlatformBrowser(this.platformId)
+          ? new ReCaptchaV3Provider(siteKey)
+          : new CustomProvider({
+              getToken: () => new Promise(() => { })
+          })
+  }
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -20,6 +41,8 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(), 
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), 
     provideFirestore(() => getFirestore()), 
+    // ...a
+    
     // provideAppCheck(() => {
     //   // const platformId = inject(PLATFORM_ID);
     //   // if (isPlatformServer(platformId)) {
@@ -30,5 +53,28 @@ export const appConfig: ApplicationConfig = {
 
       
     // }),
+
+
+    // provideAppCheck((injector) =>  {
+    //   const admin = injector.get<app.App|null>(FIREBASE_ADMIN, null);
+    //   if (admin) {
+    //     const provider = new CustomProvider({ getToken: () =>
+    //       admin.
+    //       appCheck().
+    //       createToken(environment.firebaseConfig.appId, { ttlMillis: 604_800_000, /* 1 week */ }).
+    //       then(({ token, ttlMillis: expireTimeMillis }: { token: string, ttlMillis: number }) => ({ token, expireTimeMillis } ))
+    //     });
+    //     return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: false });
+    //   } else {
+    //     const provider = new ReCaptchaV3Provider(environment.recaptcha3SiteKey);
+    //     return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
+    //   }
+    // }, [new Optional(), FIREBASE_ADMIN]),
+
+    provideAppCheck((injector) => initializeAppCheck(getApp(), {
+      provider: injector.get(RecaptchaBrowser).provider(environment.recaptcha3SiteKey),
+      isTokenAutoRefreshEnabled: true
+    })),
+
   ]
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
+    recaptcha3SiteKey: '6LcO2AwqAAAAAIQN58P9rFxf-Zl1BG90pw92KA33',
     turnstileSiteKey: '0x4AAAAAAAexZARCghRsEh7-',
     firebaseConfig: {
         apiKey: "AIzaSyAA_9ynYVWJu9_evgFZOuTPDepjDQjyIkM",
