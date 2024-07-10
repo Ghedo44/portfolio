24d80797aa62edf526dccb298db715e74de6f0b9
Ghedo <federicoghedini@gmail.com>
2024-07-10 15:12:28 +0200

    refactor: Update app.config.ts to use ReCaptchaV3Provider for Firebase app check

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,7 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
-import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+import { initializeAppCheck, provideAppCheck, ReCaptchaV3Provider } from '@angular/fire/app-check';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -18,9 +18,10 @@ export const appConfig: ApplicationConfig = {
     ), 
     provideClientHydration(), 
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), 
-    provideFirestore(() => getFirestore()), provideFirebaseApp(() => initializeApp({"projectId":"ghedo-portfolio","appId":"1:1019464200355:web:6a9ab87091071cfb15cf16","storageBucket":"ghedo-portfolio.appspot.com","apiKey":"AIzaSyAA_9ynYVWJu9_evgFZOuTPDepjDQjyIkM","authDomain":"ghedo-portfolio.firebaseapp.com","messagingSenderId":"1019464200355"})), provideAppCheck(() => {
+    provideFirestore(() => getFirestore()), 
+    provideAppCheck(() => {
       // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
-      const provider = new ReCaptchaEnterpriseProvider('6LcO2AwqAAAAAIQN58P9rFxf-Zl1BG90pw92KA33');
+      const provider = new ReCaptchaV3Provider('6LcO2AwqAAAAAIQN58P9rFxf-Zl1BG90pw92KA33');
       return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
     }),
   ]
