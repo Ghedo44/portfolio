9c26fd7596faa33bee86375a6b91c54b419035f7
Ghedo <federicoghedini@gmail.com>
2024-07-10 15:26:00 +0200

    refactor: Update app.config.ts to use ReCaptchaV3Provider for Firebase app check

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getApp, initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 import { initializeAppCheck, provideAppCheck, ReCaptchaV3Provider } from '@angular/fire/app-check';
+
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,9 +21,16 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(), 
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), 
     provideFirestore(() => getFirestore()), 
-    provideAppCheck(() => {
-      const provider = new ReCaptchaV3Provider('6LcO2AwqAAAAAIQN58P9rFxf-Zl1BG90pw92KA33');
-      return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
-    }),
   ]
 };
+
+export const browserConfig: ApplicationConfig = {
+  providers: [
+    provideAppCheck(() => {
+      const provider = new ReCaptchaV3Provider('6LcO2AwqAAAAAIQN58P9rFxf-Zl1BG90pw92KA33');
+      return initializeAppCheck(getApp(), { provider, isTokenAutoRefreshEnabled: true });
+    }),
+  ]
+}
+
+export const config = mergeApplicationConfig(appConfig, browserConfig);
