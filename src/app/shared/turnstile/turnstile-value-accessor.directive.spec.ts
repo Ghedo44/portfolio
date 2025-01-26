cd628c7047458e0a54b5e5d2cb43be1c1d62d213
Ghedo <federicoghedini@gmail.com>
2025-01-26 19:22:34 +0100

    refactor: Remove unused files and configurations for a cleaner project structure

--- src/app/shared/turnstile/turnstile-value-accessor.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TurnstileValueAccessorDirective } from './turnstile-value-accessor.directive';
-
-describe('TurnstileValueAccessorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TurnstileValueAccessorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
